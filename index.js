07a882b3e250d6fcd33521dfee18dcba8be43438
Parminder Singh <parmindersingh@sdplabs.com>
2023-06-06 17:02:13 +0530

    Commit at 5:05 PM

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send({
-		message: 'App working fine.................5 PM'
+		message: 'App working fine.................5:05 PM'
 	});
 });
 
