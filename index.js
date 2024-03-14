98506d4e54389e7f6d3307c4211864914826763f
Parminder singh <58941157+parmindersingh4385@users.noreply.github.com>
2024-03-14 12:53:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 
+const cors = require('cors');
+app.use(cors());
+
 //products model
 const PRODUCTS = mongoose.model('tbl_products', {
 	title: String,
