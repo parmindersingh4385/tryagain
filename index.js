5c9525a6e61b2d6dae67c11e59376b17ddaa14fd
Parminder singh <58941157+parmindersingh4385@users.noreply.github.com>
2024-03-21 18:44:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ mongoose
 
 app.get('/', function(req, res){
 	res.send({
-		message: 'Working fine......2222'
+		message: 'Working fine......33333'
 	});
 });
 
-app.get('/search', async function(req, res){
+app.POST('/search', async function(req, res){
     var searchText = req.query.text;
 
 	searchText = searchText.split(' ').join('+');
